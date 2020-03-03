a7e6af9c8a42a26265eb5955ca85add440e9e730
Santiago Marin <santiagomarinrendon@gmail.com>
2020-03-03 14:33:44 -0500

    Subida Codigo Ejemplo

---
+++ example/src/main/java/poo/example/App.java
@@ -0,0 +1,40 @@
+package poo.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {    	
+		//creo un nuevo objeto de tipo celular, con el constructor vacío
+		Celular celular = new Celular();
+		// le asigno la marca, modelo, y color
+		celular.setMarca("Nokia");
+		celular.setModelo("1100");
+		celular.setColor("Gris");
+		//utilizo lo métodos, llamar, cortarLlamada e informar características
+		celular.llamar("Pablo");
+		celular.cortarLlamada();
+		System.out.println();
+		celular.informarCaracteristicas();
+		////////////////////////////////////////////////////////////////////////
+		///Se crea un objeto de tipo Smartphone con polimorfismo a traves del padre 
+		Celular smartphone = new SmartPhone();
+		smartphone.setMarca("SmartPhone - Samsug");
+		smartphone.setModelo("SmartPhone - S3 MINI");
+		smartphone.setColor("SmartPhone - Negro");
+		((SmartPhone)smartphone).setPixelesCamara(5);
+		((SmartPhone)smartphone).setTamanioMemoriaInterna(16);
+		((SmartPhone)smartphone).setTamanioMemoriaExterna(8);
+		((SmartPhone)smartphone).setTamanioMemoriaRam(4);
+		//utilizo lo métodos, llamar, cortarLlamada heredados de la clase celular
+		smartphone.llamar("Carlos");
+		smartphone.cortarLlamada();
+		System.out.println();
+		
+		//método sobrescrito
+		smartphone.informarCaracteristicas();			
+	}
+}
---
+++ example/src/main/java/poo/example/SmartPhone.java
@@ -0,0 +1,67 @@
+package poo.example;
+
+public class SmartPhone extends Celular{
+
+	private float pixelesCamara;
+	private float tamanioMemoriaInterna;
+	private float tamanioMemoriaExterna;
+	private float tamanioMemoriaRam;
+	//constructor por defecto
+	public SmartPhone() {
+	}
+
+	//constructor con los atributos de la clase incluso los heredados
+	public SmartPhone(String marca, String modelo, String color, float pixelesCamara, float tamanioMemoriaInterna,
+			float tamaoDisco, float tamanioMemoriaRam) {
+		super(marca, modelo, color);
+		this.pixelesCamara = pixelesCamara;
+		this.tamanioMemoriaInterna = tamanioMemoriaInterna;
+		this.tamanioMemoriaExterna = tamaoDisco;
+		this.tamanioMemoriaRam = tamanioMemoriaRam;
+	}
+	// método sobrescrito (override), utilizo código de la clase Celular y añado código que necesito 
+	@Override
+	public void informarCaracteristicas() {
+		// TODO Auto-generated method stub
+		super.informarCaracteristicas();
+		System.out.println(String.format("SmartPhone calidad cámara: %s pixeles", pixelesCamara));
+		System.out.println(String.format("SmartPhone tamaño memoria interna: %s GB", tamanioMemoriaInterna));
+		System.out.println(String.format("SmartPhone tamaño memoria externa: %s GB", tamanioMemoriaExterna));
+		System.out.println(String.format("SmartPhone tamaño memoria Ram: %s GB", tamanioMemoriaRam));
+	}
+
+	public float getPixelesCamara() {
+		return pixelesCamara;
+	}
+
+	public void setPixelesCamara(float pixelesCamara) {
+		this.pixelesCamara = pixelesCamara;
+	}
+
+	public float getTamanioMemoriaInterna() {
+		return tamanioMemoriaInterna;
+	}
+
+	public void setTamanioMemoriaInterna(float tamanioMemoriaInterna) {
+		this.tamanioMemoriaInterna = tamanioMemoriaInterna;
+	}
+
+	public float getTamanioMemoriaExterna() {
+		return tamanioMemoriaExterna;
+	}
+
+	public void setTamanioMemoriaExterna(float tamanioMemoriaExterna) {
+		this.tamanioMemoriaExterna = tamanioMemoriaExterna;
+	}
+
+	public float getTamanioMemoriaRam() {
+		return tamanioMemoriaRam;
+	}
+
+	public void setTamanioMemoriaRam(float tamanioMemoriaRam) {
+		this.tamanioMemoriaRam = tamanioMemoriaRam;
+	}
+	
+	
+	
+}
